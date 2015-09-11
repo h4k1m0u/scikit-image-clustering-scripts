b14d8cdc82cfe4470c4d824fe143d2367999f108
Hakim Benoudjit <h.benoudjit@gmail.com>
2015-09-11 16:34:40 +0100

    - open geotiff image with gdal prior to histogram calculation

--- histogram.py
+++ histogram.py
@@ -1,25 +1,15 @@
 #!/usr/bin/env python
+import numpy as np
 import matplotlib.pyplot as plt
-import skimage.io as io
-from skimage.exposure import histogram, cumulative_distribution
-
-# 1 x 2 grid figure
-fig = plt.figure()
+from osgeo import gdal
+from skimage.exposure import histogram
 
 # display original image's channel 
-cell1 = fig.add_subplot(1, 3, 1)
-img = io.imread('flood.jpg')[:, :, 0]
-io.imshow(img)
+dataset = gdal.Open('img/mozambique-after.tiff')
+band = dataset.GetRasterBand(1)
+img = band.ReadAsArray().astype(np.uint8)
 
 # histogram of the gray scale image
-cell2 = fig.add_subplot(1, 3, 2)
 hist, bins = histogram(img)
 plt.plot(bins, hist)
-
-# accumulated histogram of the gray scale image
-cell3 = fig.add_subplot(1, 3, 3)
-hist, bins = cumulative_distribution(img)
-plt.plot(bins, hist)
-
-# show figure grid
 plt.show()
