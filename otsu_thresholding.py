31e60e6b6c3627bd963e36ae613e8f0e1b999ec1
Hakim Benoudjit <h.benoudjit@gmail.com>
2015-09-11 16:22:08 +0100

    - open geotiff image with gdal prior to thresholding

--- otsu_thresholding.py
+++ otsu_thresholding.py
@@ -1,48 +1,52 @@
 #!/usr/bin/env python
+import numpy as np
 import matplotlib.pyplot as plt
-import skimage.io as io
+from osgeo import gdal
+from skimage.transform import resize
 from skimage.filters import threshold_otsu
 from skimage.filters.rank import median, mean
 from skimage.morphology import disk
 
-# 1 x 2 grid figure
-fig = plt.figure()
+# initialize driver
+driver = gdal.GetDriverByName('GTiff')
+
+def write_image(img, filename):
+    """
+    Write img array to a file with the given filename
+    Args:
+        img (Band)
+        filename (str)
+    """
+    x_size = img.shape[1]
+    y_size = img.shape[0]
+    dataset = driver.Create(filename, x_size, y_size)
+    dataset.GetRasterBand(1).WriteArray(img)
 
 # display original image's channel
-cell1 = fig.add_subplot(3, 2, 1)
-img = io.imread('flood.jpg')[:, :, 0]
-io.imshow(img)
+dataset = gdal.Open('img/mozambique-after.tiff')
+band = dataset.GetRasterBand(1)
+img = band.ReadAsArray().astype(np.uint8)
 
 # otsu thresholding of the original image
-cell2 = fig.add_subplot(3, 2, 2)
 threshold = threshold_otsu(img)
-thresholded_img = img > threshold
+img_thresholded = img > threshold
 print 'Threshold for original image:', threshold
-io.imshow(thresholded_img)
+write_image(img_thresholded, 'img/mozambique-after-thresholded.tiff')
 
-# display original image filtered with a mean filter
-cell3 = fig.add_subplot(3, 2, 3)
-mean_img = mean(img, disk(11))
-io.imshow(mean_img)
+# image filtered with a mean filter
+img_mean = mean(img, disk(1))
 
 # otsu thresholding of the mean-filtered image
-cell4 = fig.add_subplot(3, 2, 4)
-threshold = threshold_otsu(mean_img)
-thresholded_mean_img = mean_img > threshold
+threshold = threshold_otsu(img_mean)
+img_mean_thresholded = img_mean > threshold
 print 'Threshold for Mean-filtered image :', threshold
-io.imshow(thresholded_mean_img)
+write_image(img_mean_thresholded, 'img/mozambique-after-mean-thresholded.tiff')
 
-# display original image filtered with a median filter
-cell5 = fig.add_subplot(3, 2, 5)
-median_img = median(img, disk(11))
-io.imshow(median_img)
+# image filtered with a median filter
+img_median = median(img, disk(1))
 
 # otsu thresholding of the median-filtered image
-cell6 = fig.add_subplot(3, 2, 6)
-threshold = threshold_otsu(median_img)
-thresholded_median_img = median_img > threshold
+threshold = threshold_otsu(img_median)
+img_median_thresholded = img_median > threshold
 print 'Threshold for Median-filtered image:', threshold
-io.imshow(thresholded_median_img)
-
-# show figure grid
-plt.show()
+write_image(img_median_thresholded, 'img/mozambique-after-median-thresholded.tiff')
