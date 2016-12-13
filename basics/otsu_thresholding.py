cc320a928004df9763d0896393be99fc9d963f75
Hakim Benoudjit <h.benoudjit@gmail.com>
2016-12-13 15:56:17 +0000

    Apply split-based thresholding on image of Tewkesbury 2007

--- basics/otsu_thresholding.py
+++ basics/otsu_thresholding.py
@@ -4,10 +4,13 @@ from geotiff.io import IO
 
 
 # load original image
-img = IO.read('C:/Data/namibia-flipped.data/Sigma0_HH.img')
+PATH = 'C:/Data/Tewkesbury-SAR/building-reconstruction'
+IN_PATH = PATH + '/stack.data/Sigma0_HH_mst_25Jul2007_db.img'
+OUT_PATH = PATH + '/tewkesbury-thresholded.tif'
+img = IO.read(IN_PATH)
 
 # otsu thresholding of the original image
 threshold = threshold_otsu(img)
 img_thresholded = img > threshold
 print 'Threshold for original image:', threshold
-IO.write(img_thresholded, 'C:/Data/namibia-flipped-thresholded.tif')
+IO.write(img_thresholded, OUT_PATH)
