6c688ab6ffb7c6ec992d71daaafd64f1f65b527f
Benoudjit Hakim <h.benoudjit@gmail.com>
2017-11-27 17:07:25 +0000

    Kmeans clustering of DTM

--- clustering/kmeans_clustering.py
+++ clustering/kmeans_clustering.py
@@ -1,32 +1,27 @@
 #!/usr/bin/env python
-import numpy as np
-from skimage.filters import threshold_otsu
 from sklearn import cluster
-from geotiff.io import IO
+from gdal_io import IO
 
 
-# load original image
-DIR = 'C:/Data/Tewkesbury-LiDAR'
-img = IO.read(DIR + '/stack-lidar.data/Sigma0_HH_slv1_25Jul2007.img')
+# load dtm image
+path_in = '/'.join(('C:', 'Data', 'Tewkesbury-Mask',
+                    'SAR_elevation_mask_subset.data', 'band_1_S.img'))
+data = IO.read(path_in)
+img = data.array
 
 # k-means clustering of the image (population of pixels intensities)
-# cluster pixel intensities using k-means
 X = img.reshape((-1, 1))
 k_means = cluster.KMeans(n_clusters=2)
 k_means.fit(X)
 
-# extract means of each cluster & clustered intensities population
+# extract means of each cluster & clustered population
 clusters_means = k_means.cluster_centers_.squeeze()
 X_clustered = k_means.labels_
-print '# of Observations:', X.shape
-print 'Clusters Means:', clusters_means
+print('# of Observations:', X.shape)
+print('Clusters Means:', clusters_means)
 
-# get clustered image from clustered intensities
-img_clustered = np.choose(X_clustered, (0, 1))
-img_clustered.shape = img.shape
-IO.write(img_clustered, DIR + '/kmeans.tif')
-
-# otsu thresholding of the binary image obtained
-threshold = threshold_otsu(img_clustered)
-img_thresholded = img_clustered > threshold
-IO.write(img_thresholded, DIR + '/kmeans-thresholding.tif')
+# save clustered image
+X_clustered.shape = img.shape
+path_out = '/'.join(('C:', 'Data', 'Tewkesbury-Mask', 'kmeans.tif'))
+IO.write(X_clustered, path_out, projection=data.projection,
+         geocoordinates=data.geocoordinates)
